5469a266c9ead1c36ccce4a4a8eee5ff9ef0766e
Lucas Kaique <lucaskaique743@gmail.com>
2023-04-04 21:02:14 -0300

    Adcionando estilos css no projeto e alterando a fonte

--- Lista-presença/src/pages/Home/index.jsx
+++ Lista-presença/src/pages/Home/index.jsx
@@ -3,11 +3,11 @@ import './style.css'
 
 function Home() {
   return (
-    <>
+    <div className='container'>
       <h1>Lista de Presença</h1>
       <input type="text" placeholder="Digite o nome..." />
       <button type="buttonn">Adcionar</button>
-    </>
+    </div>
   )
 }
 
